b932ea42b51f96c380ec8b4552bf7d168af5ea5a
Norman Lorrain <normanlorrain@gmail.com>
2020-02-15 12:01:18 -0700

    Better interface to Header/Footer.

--- mdpdf/headfoot.py
+++ mdpdf/headfoot.py
@@ -4,16 +4,16 @@ class Base:
         self._left, self._mid, self._right = fmt.split(",")
 
     @classmethod
-    def left(cls, **metaData):
-        return cls._left.format(**metaData)
+    def left(cls, metaData):
+        return cls._left.format(**metaData.__dict__)
 
     @classmethod
-    def mid(cls, **metaData):
-        return cls._mid.format(**metaData)
+    def mid(cls, metaData):
+        return cls._mid.format(**metaData.__dict__)
 
     @classmethod
-    def right(cls, **metaData):
-        return cls._right.format(**metaData)
+    def right(cls, metaData):
+        return cls._right.format(**metaData.__dict__)
 
 
 class Header(Base):
@@ -24,20 +24,25 @@ class Foot(Base):
     pass
 
 
+class MetaData(object):
+    pass
+
+
 if __name__ == "__main__":
     Header.setFmt("{date},blah,{page}")
 
-    date = "2020-02-15"
-    page = 12
-    header = "chapter 1"
+    metaData = MetaData()
+    metaData.date = "2020-02-15"
+    metaData.page = 12
+    metaData.header = "chapter 1"
 
-    metaData = {"date": date, "page": page, "header": header}
+    # metaData = {"date": date, "page": page, "header": header}
 
-    l = Header.left(**metaData)
-    m = Header.mid(**metaData)
-    r = Header.right(**metaData)
+    l = Header.left(metaData)
+    m = Header.mid(metaData)
+    r = Header.right(metaData)
 
-    metaData["page"] += 1
-    r = Header.right(**metaData)
+    metaData.page += 1
+    r = Header.right(metaData)
 
     pass

f2ccd4e5c21c88e79c97a13be7719aa20c9c1bdd
Norman Lorrain <normanlorrain@gmail.com>
2020-03-03 11:06:45 -0700

    Refactoring of tests.

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -3,34 +3,9 @@ from pathlib import Path
 
 from mdpdf.converter import convertMarkdown2Pdf
 
-indir = Path(__file__).parent.absolute() / "input"
-outdir = Path(__file__).parent.absolute() / "output"
-outdir.mkdir(exist_ok=True)
-
-
-def test_entire_spec():
-    infilename = indir / "spec.txt"
-    outfilename = outdir / "spec_raw.pdf"
-    convertMarkdown2Pdf(infilename, outfilename)
-
-
-def test_entire_spec_with_examples():
-    import spec_to_md
-
-    spec = indir / "spec.txt"
-    md = outdir / "spec.md"
-    pdf = outdir / "spec_examples.pdf"
-    spec_to_md.convertSpecExamples(spec, md)
-    convertMarkdown2Pdf(md, pdf)
-
-
-def test_quick():
-    infilename = indir / "quick.md"
-    outfilename = outdir / "quick.pdf"
-    convertMarkdown2Pdf(infilename, outfilename)
+# See test_examples_files.py
 
 
 if __name__ == "__main__":
 
     test_quick()
-
--- tests/test_example_files.py
+++ tests/test_example_files.py
@@ -1,9 +1,8 @@
 import pytest
+
 from pathlib import Path
 import mdpdf.cli as cli
 from click.testing import CliRunner, Result
-
-# pytestmark = pytest.mark.usefixtures("convertSpec")
 
 
 def inPath(name):
@@ -18,27 +17,13 @@ def outPath(name):
     return str(fullpath)
 
 
-@pytest.fixture(scope="module")
-def convertSpec(infile):
-    b = r"````+ example\n(.*?)^\.(.*?````+)"  # Note : https://regex101.com/r/wsVyL9/1
-    regex = re.compile(b, flags=re.DOTALL | re.MULTILINE,)
-    text = open(infile, encoding="utf-8").read()
-    s = r"\n```\nExample:\n\1\n```\n\1\n"
-    s = r"""
-```
-Example:
-\1
-```
----
-\1
----"""
-    newtext = regex.sub(s, text)
-    open(outfile, "w", encoding="utf-8").write(newtext)
-    return outfile
-
-
 @pytest.mark.parametrize(
-    "inFile,outFile", [("quick.md", "quick.pdf"), ("lorem.md", "lorem.pdf")]
+    "inFile,outFile",
+    [
+        ("quick.md", "quick.pdf"),
+        ("lorem.md", "lorem.pdf"),
+        ("spec.txt.md", "spec.pdf"),
+    ],
 )
 def test_example_Files(inFile, outFile):
     print(inFile, outFile)

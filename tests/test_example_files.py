8f313dae3e8280b3beb3db4712cbdc68276190e0
Norman Lorrain <normanlorrain@gmail.com>
2021-02-20 11:04:56 -0700

    Trying to diagnose problem with logging and click (sometimes object deletion fails and causes a crash .

--- tests/test_example_files.py
+++ tests/test_example_files.py
@@ -2,7 +2,13 @@ import pytest
 
 from pathlib import Path
 import mdpdf.cli as cli
+import mdpdf.log as log
+
 from click.testing import CliRunner, Result
+
+
+#TODO: pytest-console-scripts
+
 
 
 def inPath(name):
@@ -16,6 +22,9 @@ def outPath(name):
     fullpath /= name
     return str(fullpath)
 
+@pytest.fixture(scope="module")
+def runner():
+    return CliRunner()
 
 @pytest.mark.parametrize(
     "inFile,outFile",
@@ -25,8 +34,7 @@ def outPath(name):
         ("spec.txt.md", "spec.pdf"),
     ],
 )
-def test_example_Files(inFile, outFile):
+def test_example_Files(inFile, outFile, runner):
     print(inFile, outFile)
-    runner: CliRunner = CliRunner()
-    result: Result = runner.invoke(cli.cli, ["-o", outPath(outFile), inPath(inFile)])
+    result = runner.invoke(cli.cli, ["-o", outPath(outFile), inPath(inFile)])
     print(result)
